1886d6ff28948fb9614b61e026f6a760e3b4e960
MizanHM <mizanhabte924@gmail.com>
2024-07-09 19:37:05 +0300

    Add files via upload

---
+++ april 25.py
@@ -0,0 +1,35 @@
+#num=(int(input("enter a number")))
+#if num %2==0:
+    #    print("even")
+#else:
+ #  print("odd")
+
+#A=(int(input("the first number")))
+#B= (int(input("the second number")))
+#C=(int(input("the third number")))
+#if A > B and A > C:
+ #   print (A, "is the largest")
+#elif B > C:
+ #   print (B, "is the largest")
+#else:
+ #3   print(C, "is the largest")
+
+
+#letter=(str(input("enter a letter here")))
+#if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+ #   print(letter, "is a vowle")
+#else:
+#    print(letter,"is consonant")
+ 
+#salary= (input("enter your salary")))
+#if salary <=1650
+
+#grade=(str(input("enter your greade")))
+#if grade == "A":
+    #print ("exellent")
+#elif grade =="B":
+   # print("Very good")
+#elif grade == "C":
+ #   print ("good")
+#else:
+#    print("fail")
